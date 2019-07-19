e6420557df7c3af26b62863ac8824c3f72485097
Mahesh Prasad <mahesh223prasad@gmail.com>
2019-07-19 11:14:41 +0530

    Create static UITableView

---
+++ TableView Tutorial/Controllers/ProductDetailTableViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ProductDetailTableViewController.swift
+//  TableView Tutorial
+//
+//  Created by Mahesh Prasad on 19/07/19.
+//  Copyright © 2019 Mahesh Prasad. All rights reserved.
+//
+
+import UIKit
+
+class ProductDetailTableViewController: UITableViewController {
+
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productTitleTextField: UITextField!
+    @IBOutlet weak var productDescriptionTextView: UITextView!
+    
+    var product: Product? = ProductLine.getProductLines()[0].product[0]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Edit Product"
+        
+        productImageView.image = product?.image
+        productTitleTextField.text = product?.title
+        productTitleTextField.delegate = self
+        productDescriptionTextView.text = product?.description
+    }
+    
+    //MARK: - UITableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            return indexPath
+        }else {
+            return nil
+        }
+    }
+}
+
+
+//Dismiss the keyboard after click return button from keyboard
+extension ProductDetailTableViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+//MARK: UIScrollViewDelegate
+
+extension ProductDetailTableViewController
+{
+    override func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        productDescriptionTextView.resignFirstResponder()
+        productTitleTextField.resignFirstResponder()
+    }
+}
